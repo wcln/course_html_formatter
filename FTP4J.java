2253ddc937bfd99ff8805df757b0eb4f698aae99
Colin Perepelken <colin@perepelken.ca>
2022-01-10 18:11:40 -0800

    Update jquery, bootstrap, and createjs sources

--- FTP4J.java
+++ FTP4J.java
@@ -1,4 +1,4 @@
-package bernard.colin;
+package Perepelken.colin;
 
 import java.io.File;
 import java.io.IOException;
--- Formatter.java
+++ Formatter.java
@@ -1,4 +1,4 @@
-package bernard.colin;
+package Perepelken.colin;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
